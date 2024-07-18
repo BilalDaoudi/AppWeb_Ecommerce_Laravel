805ce67a633a1758f9f399976635358c5b22f1a1
Bilal Daoudi <105610472+BilalDaoudi@users.noreply.github.com>
2024-07-18 18:39:27 +0100

    Update 2023_07_04_204421_create_tests_table.php

--- database/migrations/2023_07_04_204421_create_tests_table.php
+++ database/migrations/2023_07_04_204421_create_tests_table.php
@@ -6,7 +6,6 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    
     public function up(): void
     {
         Schema::create('tests', function (Blueprint $table) {
@@ -14,7 +13,6 @@ return new class extends Migration
             $table->timestamps();
         });
     }
-
     /**
      * Reverse the migrations.
      */

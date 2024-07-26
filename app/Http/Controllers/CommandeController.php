f5ea88db98e1840602c1fd30a96881f0795bcfa5
Bilal Daoudi <105610472+BilalDaoudi@users.noreply.github.com>
2024-07-26 20:05:51 +0100

    Update CommandeController.php

--- app/Http/Controllers/CommandeController.php
+++ app/Http/Controllers/CommandeController.php
@@ -7,6 +7,7 @@ use App\Models\LigneCommande;
 use App\Models\LigneCommandeDemo;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
+
 class CommandeController extends Controller
 {
     public function __construct()

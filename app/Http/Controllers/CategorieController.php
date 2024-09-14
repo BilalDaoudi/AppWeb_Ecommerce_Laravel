d587621cc8a388b0cb68e9f9935ddda823e3953e
Bilal Daoudi <105610472+BilalDaoudi@users.noreply.github.com>
2024-09-14 15:51:48 +0100

    Update CategorieController.php

--- app/Http/Controllers/CategorieController.php
+++ app/Http/Controllers/CategorieController.php
@@ -7,25 +7,30 @@ use Illuminate\Support\Facades\Gate;
 
 class CategorieController extends Controller
 {
+    
     public function __construct(){
         $this->middleware('auth');
     }
+    
     public function index()
     {
         Gate::authorize("IsAdmin");
         $categories = Categorie::paginate(7);
         return view('categories.index', compact('categories'));
     }
+    
     public function create()
     {
         Gate::authorize("IsAdmin");
         return view('categories.create');
     }
+    
     public function show(Categorie $categorie){
         Gate::authorize("IsAdmin");
         $produits = $categorie->produits;
         return view('categories.show', compact('categorie',"produits"));
     }
+    
     public function store(CategorieRequest $request)
     {
         Gate::authorize("IsAdmin");
@@ -35,11 +40,13 @@ class CategorieController extends Controller
 
         return redirect()->route("categories.index")->with('success', 'La catégorie a été créée avec succès.');
     }
+    
     public function edit(Categorie $categorie)
     {
         Gate::authorize("IsAdmin");
         return view('categories.edit', compact('categorie'));
     }
+    
     public function update(CategorieRequest $request, Categorie $categorie)
     {
         Gate::authorize("IsAdmin");
@@ -50,12 +57,14 @@ class CategorieController extends Controller
         return redirect()->route("categories.index")->with('success', 'La catégorie a été mise à jour avec succès.');
     }
 
+    
     public function destroy(Categorie $categorie)
     {
         Gate::authorize("IsAdmin");
         $categorie->delete();
         return back()->with('success', 'La catégorie a été supprimée avec succès.');
     }
+    
 }
 
 

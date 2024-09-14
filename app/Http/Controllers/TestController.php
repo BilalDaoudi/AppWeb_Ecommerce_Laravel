4031025fff7a8aa69f5f6a2ca780541bfbdaa33f
Bilal Daoudi <105610472+BilalDaoudi@users.noreply.github.com>
2024-09-14 15:56:21 +0100

    Update TestController.php

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -7,43 +7,5 @@ use App\Http\Requests\UpdateTestRequest;
 
 class TestController extends Controller
 {
-   
-    public function index()
-    {
-        //
-    }
-
-    public function create()
-    {
-        //
-    }
-
-    public function store(StoreTestRequest $request)
-    {
-        //
-    }
-
-
-    public function show(Test $test)
-    {
-        //
-    }
-
- 
-    public function edit(Test $test)
-    {
-        //
-    }
-
-
-    public function update(UpdateTestRequest $request, Test $test)
-    {
-        //
-    }
-
-
-    public function destroy(Test $test)
-    {
-        //
-    }
+    //
 }

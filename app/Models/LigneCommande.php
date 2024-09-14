12082ff63c9db8badc0928583c9475a4a3fc5e7e
Bilal Daoudi <105610472+BilalDaoudi@users.noreply.github.com>
2024-09-14 15:58:44 +0100

    Update LigneCommande.php

--- app/Models/LigneCommande.php
+++ app/Models/LigneCommande.php
@@ -1,22 +1,16 @@
 <?php
 
 namespace App\Models;
-
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class LigneCommande extends Model
 {
-    
     protected $table = "ligne_commandes";
-    
     protected $fillable = ["id_commande","codeproduit","quantite"];
-    
     use HasFactory;
-    
     public function produit()
     {
-        
         return $this->belongsTo(Produit::class,"codeproduit");
     }
 }

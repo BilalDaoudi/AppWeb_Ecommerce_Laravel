24f42b8144d3516e5c1140280056c518101af8fe
Bilal Daoudi <105610472+BilalDaoudi@users.noreply.github.com>
2025-01-23 01:53:22 +0100

    Update LoginController.php

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -5,7 +5,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Session;
 
-
 class LoginController extends Controller
 {
     public function show()

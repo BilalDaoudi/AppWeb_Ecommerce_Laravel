15230b1f7cfb95c6108ccdfc761d9bf85dee9aaf
Bilal Daoudi <105610472+BilalDaoudi@users.noreply.github.com>
2025-04-16 17:06:00 +0000

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,8 +6,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Hash;
 
-
-
 class UserController extends Controller
 {
     public function index()
